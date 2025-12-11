9b78291387e069a7913b9a6e8cbff9c3ab4b7277
s20233085 <20233085-nakayamashinnosuke@hcs.ac.jp>
2025-12-11 11:37:53 +0900

    interview main画面修正

--- src/main/kotlin/jp/sabakan/mirai/controller/InterviewController.kt
+++ src/main/kotlin/jp/sabakan/mirai/controller/InterviewController.kt
@@ -7,12 +7,12 @@ import org.springframework.web.bind.annotation.PostMapping
 @Controller
 class InterviewController {
 
-    @GetMapping("/interview/interview-main")
+    @GetMapping("/interview-main")
     fun getIndex(): String {
         return "/interview/interview-main"
     }
 
-    @PostMapping("/interview/interview-main")
+    @PostMapping("/interview-main")
     fun postIndex(): String {
         return "/interview/interview-main"
     }
