6e5f9fc4ead519a613378fd4e2101a0b5f526d74
iwaisora <Le181ciel@gmail.com>
2025-12-16 15:04:22 +0900

    html訂正

--- src/main/kotlin/jp/sabakan/mirai/controller/UserController.kt
+++ src/main/kotlin/jp/sabakan/mirai/controller/UserController.kt
@@ -1,21 +1,45 @@
 package jp.sabakan.mirai.controller
 
 import org.springframework.stereotype.Controller
-import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @Controller
 class UserController {
 
-    @GetMapping("/user/profile")
-    fun userProfile(model: Model): String {
-        //データ取得
-        var hoge: String = "moge"
-
-        // データ受け渡し
-        model.addAttribute("null", null)
-
-        // 画面表示
+    // ユーザーメイン画面
+    @GetMapping("/user")
+    fun getUser(): String {
         return "users/user-main"
+    }
+
+    // パスワード変更画面
+    @GetMapping("/user/repassword")
+    fun getRepassword(): String {
+        return "users/user-repassword"
+    }
+
+    // ユーザー目標設定画面
+    @GetMapping("/user/target")
+    fun getTarget(): String {
+        return "users/user-target"
+    }
+
+    // ユーザーメイン画面
+    @PostMapping("/user")
+    fun postUser(): String{
+        return "users/user-main"
+    }
+
+    // パスワード変更画面
+    @PostMapping("/user/repassword")
+    fun postRepassword(): String{
+        return "users/user-repassword"
+    }
+
+    // ユーザー目標設定画面
+    @PostMapping("/user/target")
+    fun postTarget(): String{
+        return "users/user-target"
     }
 }
