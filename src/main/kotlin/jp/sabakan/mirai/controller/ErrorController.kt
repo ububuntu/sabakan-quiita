7f705782b6df22e8e4d7ecb1a6c6badd9ab7d5ba
iwaisora <Le181ciel@gmail.com>
2025-12-17 11:40:27 +0900

    controller修正

--- src/main/kotlin/jp/sabakan/mirai/controller/ErrorController.kt
+++ src/main/kotlin/jp/sabakan/mirai/controller/ErrorController.kt
@@ -3,7 +3,6 @@ package jp.sabakan.mirai.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
 
 
 @Controller
@@ -15,33 +14,15 @@ class ErrorController {
         return "error/error"
     }
 
-    // 404エラー画面
-    @GetMapping("/error/404")
+    // 400エラー画面
+    @GetMapping("/error/400")
     fun get404Error(): String {
-        return "error/404-error"
+        return "400-error"
     }
 
     // 500エラー画面
     @GetMapping("/error/500")
     fun get500Error(): String {
-        return "error/500-error"
-    }
-
-    // 共通のエラー画面
-    @PostMapping("/errors")
-    fun postError(): String {
-        return "error/error"
-    }
-
-    // 404エラー画面
-    @PostMapping("/error/404")
-    fun post404Error(): String {
-        return "error/404-error"
-    }
-
-    // 500エラー画面
-    @PostMapping("/error/500")
-    fun post500Error(): String {
         return "error/500-error"
     }
 }
